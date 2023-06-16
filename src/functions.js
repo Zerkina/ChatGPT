4ccb2845342c59bf02e0924fd90f4eca098452e6
naszerkina@yandex.ru <naszerkina@yandex.ru>
2023-06-16 13:04:42 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,14 +1,13 @@
-function CheckData(phoneNumber){
+function Chat(){
     var url = "https://10.131.0.14:2020/api/send_text";
            
     var options = {
                     dataType: "json",
                     headers: {
                             "Content-Type": "application/json"
-                            "text": "Специальность врача: терапевт. Время приема: Пн 15:00, Вт 14:00, Ср 09:00, Чт 16:40, Пт 18:20, Сб и Вс выходные." }'
                             },
-                    form: {
-                            phone: phoneNumber
+                    body: {
+                            "text": "Специальность врача: терапевт. Время приема: Пн 15:00, Вт 14:00, Ср 09:00, Чт 16:40,
                         }
                     };
                                     	
